676a58170c2ca4d27af8a0939f34ce3c41c74af4
Siti Jasmin <siti23si@mahasiswa.pcr.ac.id>
2025-05-21 14:54:35 +0700

    Starter Project Done by Jasmin

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from './Pages/Dashboard'
+import Sidebar from './components/Sidebar'
+import MainLayout from './components/MainLayout'
+
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Dashboard />}/>
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
